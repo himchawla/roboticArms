d0381bf54082ec77ae30e9b42eff3c5181ecc299
himchawla <himchawla43@gmail.com>
2021-01-19 02:31:08 +1300

    input from file

--- Project3/Source.cpp
+++ Project3/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<vector>
+#include<fstream>
 
 struct point
 {
@@ -12,32 +13,43 @@ struct task {
 	std::vector<point> assemblyPoints;
 };
 
+
+
 int main()
 {
+
+	std::ifstream file;
+	file.open("files/a_example.txt");
+
+
 	int W, H, R, M, T, L;
 	std::vector<point> mountPoints;
 
-	std::cin >> W >> H >> R >> M >> T >> L;
+	file >> W >> H >> R >> M >> T >> L;
 
 	for (int i = 0; i < M; i++)
 	{
 		point temp;
-		std::cin >> temp.x >> temp.y;
+		file >> temp.x >> temp.y;
 		mountPoints.push_back(temp);
 	}
 
 	for (int i = 0; i < T; i++)
 	{
 		task temp;
-		std::cin >> temp.score >> temp.numberOfPoints;
+		file >> temp.score >> temp.numberOfPoints;
 		for (int j = 0; j < temp.numberOfPoints; j++)
 		{
 			point tempPoint;
-			std::cin >> tempPoint.x >> tempPoint.y;
+			file >> tempPoint.x >> tempPoint.y;
 			temp.assemblyPoints.push_back(tempPoint);
 
 		}
 	}
+
+	std::cout << W;
+
+
 
 	return 0;
 }
